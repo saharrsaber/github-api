5939439cf7288ed214762a29ba5538e47a719d00
Sahar Saber <48351549+saharrsaber@users.noreply.github.com>
2021-01-26 15:57:42 +0200

    parallel fetching using promise.all()

--- script.js
+++ script.js
@@ -16,57 +16,23 @@ getReposBtn.addEventListener("click", function () {
     userSection.classList.remove("hidden");
     reposSection.classList.remove("hidden");
     //fetch user data
-    fetch(`https://api.github.com/users/${username.value}`)
-      .then((rsp) => rsp.json())
-      .then((data) => {
-        if (data.message == "Not Found")
-          displayError(`This username ${username.value} is not found`);
-        else {
-          userSection.insertAdjacentHTML(
-            "beforeend",
-            `
-      <div class="user-name">Name: <span>${
-        data.name === null ? "No data" : data.name
-      }</span></div>
-      <div class="user-email">Email: <span>${
-        data.email === null ? "No data" : data.email
-      }</span></div>
-      <div class="user-location">Location: <span>${
-        data.location === null ? "No data" : data.location
-      }</span></div>
-      <div class="user-followers"><span>${data.followers}</span> followers</div>
-      <div class="user-repos-num"><span>${data.public_repos}</span> repos</div>
-      `
-          );
-          return fetch(`https://api.github.com/users/${username.value}/repos`);
-        }
-      })
-      .then((rsp) => rsp.json())
-      .then((data) => {
-        if (data.message == "Not Found")
-          displayError(`This username ${username.value} is not found`);
-        else {
-          data.forEach((repo) => {
-            repo.name;
+    fetchAllData(username.value);
 
-            reposSection.insertAdjacentHTML(
-              "beforeend",
-              `
-            <div class = "repo-box">${repo.name}
-                <a href =  ${repo.html_url} target= "_blank">Visit</a>
-                <span>Stars: ${repo.stargazers_count}</span>
-                <span class="languague">${
-                  repo.language == null ? "Not Detected" : repo.language
-                }</span>
-            </div>
-            `
-            );
-          });
-        }
-      })
-      .catch(() => displayError("An error occured"));
   }
 });
+
+const getJSON =  function(url, errorMsg = "An error occurred"){
+  return fetch(url)
+  .then(response => {
+    if (!response.ok) 
+      {
+        displayError(`${errorMsg} (${response.status})`);
+        throw new Error("An error occurred");
+      }    
+    return response.json();
+  });
+}
+
 
 function displayError(msg) {
   searchSection.classList.add("hidden");
@@ -74,4 +40,50 @@ function displayError(msg) {
   reposSection.classList.add("hidden");
   errorSection.classList.remove("hidden");
   errorSection.innerHTML = `<span>${msg}</span>`;
+}
+
+
+const fetchAllData = async function(username){
+  try
+  {
+    const data = await Promise.all([
+      getJSON(`https://api.github.com/users/${username}`, `This username ${username} is not found`),
+      getJSON(`https://api.github.com/users/${username}/repos`, `This username ${username} is not found`)
+    ]);
+    //general inforamtin section
+    userSection.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="user-name">Name: <span>${
+      data[0].name === null ? "No data" : data[0].name
+      }</span></div>
+      <div class="user-email">Email: <span>${
+      data[0].email === null ? "No data" : data[0].email
+      }</span></div>
+      <div class="user-location">Location: <span>${
+      data[0].location === null ? "No data" : data[0].location
+      }</span></div>
+      <div class="user-followers"><span>${data[0].followers}</span> followers</div>
+      <div class="user-repos-num"><span>${data[0].public_repos}</span> repos</div>
+      `
+      );
+    // repo section 
+    data[1].forEach((repo) => {
+
+      reposSection.insertAdjacentHTML(
+        "beforeend",
+        `
+      <div class = "repo-box">${repo.name}
+          <a href =  ${repo.html_url} target= "_blank">Visit</a>
+          <span>Stars: ${repo.stargazers_count}</span>
+          <span class="languague">${
+            repo.language == null ? "Not Detected" : repo.language
+          }</span>
+      </div>
+      `
+      );
+    });
+  } catch(err){
+    console.error(err);
+  }  
 }
